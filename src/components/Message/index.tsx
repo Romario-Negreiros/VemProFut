0340af1ee4f126a193fd0952c2591fa5d952f7ac
Romario Negreiros <nromario482@gmail.com>
2023-08-28 20:34:46 -0300

    Make close prop optional, and if not passed use nextjs router to add a go back button

--- src/components/Message/index.tsx
+++ src/components/Message/index.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useRouter } from "next/router";
+
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
@@ -7,15 +11,17 @@ import type { FC } from "react";
 
 interface Props {
   msg: string;
-  close: () => void;
+  close?: () => void;
 }
 
 const Message: FC<Props> = ({ msg, close }) => {
+  const { back } = useRouter();
+
   return (
     <Box sx={{ display: "grid", placeItems: "center", height: "calc(100vh - 64px)" }}>
       <Paper>
         <Typography variant="body1">{msg}</Typography>
-        <Button variant="outlined" onClick={close}>Fechar</Button>
+        {close !== undefined ? <Button variant="outlined" onClick={close}>Fechar</Button> : <Button variant="outlined" onClick={back}>Voltar</Button>}
       </Paper>
     </Box>
   )
