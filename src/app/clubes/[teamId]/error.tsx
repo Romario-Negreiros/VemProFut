d179ce572a6f49b42f140d82d969fb7f63b86191
Romario Negreiros <nromario482@gmail.com>
2023-08-28 22:17:57 -0300

    Rename dynamic parameter to teamId and create error page

---
+++ src/app/clubes/[teamId]/error.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useEffect } from "react";
+
+import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
+export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <Container sx={{ display: "grid", placeItems: "center", height: "calc(100vh - 64px)" }}>
+      <Paper sx={{ display: "flex", flexDirection: "column", gap: 2, padding: 3 }} elevation={2}>
+        <Typography variant="h2">Algo deu errado ao carregar os dados do time!</Typography>
+        <Button variant="outlined" onClick={reset}>
+          Tentar novamente
+        </Button>
+      </Paper>
+    </Container>
+  );
+}
