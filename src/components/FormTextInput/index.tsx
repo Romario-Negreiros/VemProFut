d4178337552a224a882ffe05d280c55b4631b26a
Romario Negreiros <nromario482@gmail.com>
2023-09-07 16:10:38 -0300

    Add hide/show mechanism to inputs of type password

--- src/components/FormTextInput/index.tsx
+++ src/components/FormTextInput/index.tsx
@@ -1,5 +1,12 @@
+"use client";
+
+import { useState } from "react";
 import { Controller } from "react-hook-form";
 import TextField from "@mui/material/TextField";
+import InputAdornment from "@mui/material/InputAdornment";
+import IconButton from "@mui/material/IconButton";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
 import type { FC, HTMLInputTypeAttribute } from "react";
 import type { Control, RegisterOptions } from "react-hook-form";
@@ -16,6 +23,8 @@ interface Props {
 }
 
 const FormTextInput: FC<Props> = ({ name, control, type, label, rules, disabled, inputProps }) => {
+  const [isVisible, setIsVisible] = useState(true);
+
   return (
     <Controller
       name={name}
@@ -29,9 +38,19 @@ const FormTextInput: FC<Props> = ({ name, control, type, label, rules, disabled,
           error={!(error === undefined)}
           helperText={error?.message}
           variant="filled"
-          type={type}
+          type={type === "password" ? (isVisible ? "text" : "password") : type}
           label={label}
           disabled={disabled}
+          InputProps={{
+            endAdornment:
+              type === "password" ? (
+                <InputAdornment position="end">
+                  <IconButton onClick={() => setIsVisible(!isVisible)}>
+                    {isVisible ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                  </IconButton>
+                </InputAdornment>
+              ) : undefined,
+          }}
           inputProps={inputProps}
           fullWidth
         />
