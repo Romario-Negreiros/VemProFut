d3d32c721e19c4afdb12784fc6e8d7823098aeb8
Romario Negreiros <nromario482@gmail.com>
2023-09-08 21:56:11 -0300

    Add link and export Props interface

--- src/components/Message/index.tsx
+++ src/components/Message/index.tsx
@@ -6,25 +6,45 @@ import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import Link from "@mui/material/Link";
+import NextLink from "next/link";
 
 import type { FC } from "react";
 
-interface Props {
+export interface Props {
   msg: string;
   close?: () => void;
+  link?: {
+    href: string;
+    text: string;
+  };
 }
 
-const Message: FC<Props> = ({ msg, close }) => {
+const Message: FC<Props> = ({ msg, close, link }) => {
   const { back } = useRouter();
 
   return (
     <Box sx={{ display: "grid", placeItems: "center", height: "calc(100vh - 64px)" }}>
       <Paper>
         <Typography variant="body1">{msg}</Typography>
-        {close !== undefined ? <Button variant="outlined" onClick={close}>Fechar</Button> : <Button variant="outlined" onClick={back}>Voltar</Button>}
+        <br />
+        {link ? (
+          <Link component={NextLink} href={link.href}>
+            {link.text}
+          </Link>
+        ) : null}
+        {close ? (
+          <Button variant="outlined" onClick={close}>
+            Fechar
+          </Button>
+        ) : (
+          <Button variant="outlined" onClick={back}>
+            Voltar
+          </Button>
+        )}
       </Paper>
     </Box>
-  )
+  );
 };
 
 export default Message;
