198a65abf9e8dfd74f2c8653ce236ff8a4e050ab
Romario Negreiros <nromario482@gmail.com>
2023-09-10 23:32:01 -0300

    Add UserTeams interface

--- src/contexts/userContext.ts
+++ src/contexts/userContext.ts
@@ -2,29 +2,31 @@ import { createContext } from "react";
 
 import type { Dispatch, SetStateAction } from "react";
 
+export interface UserTeam {
+  id?: number;
+  name?: string;
+  code?: string;
+  country?: string;
+  translatedCountry?: string;
+  countryFlag?: string;
+  founded?: number;
+  logo?: string;
+  venue: {
+    id?: number;
+    name?: string;
+    address?: string;
+    city?: string;
+    capacity?: number;
+    surface?: string;
+    image?: string;
+  };
+}
+
 export interface User {
   id: number;
   name: string;
   email: string;
-  teams: Array<{
-    id?: number;
-    name?: string;
-    code?: string;
-    country?: string;
-    translatedCountry?: string;
-    countryFlag?: string;
-    founded?: number;
-    logo?: string;
-    venue: {
-      id?: number;
-      name?: string;
-      address?: string;
-      city?: string;
-      capacity?: number;
-      surface?: string;
-      image?: string;
-    };
-  }>;
+  teams: UserTeam[];
   createdAt: string;
   isActive: number;
 }
