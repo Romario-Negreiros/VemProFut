d8f465a72d6b84ad37b3590aee9543c7f357bd14
Romario Negreiros <nromario482@gmail.com>
2023-09-12 15:00:59 -0300

    Add new domain to images array

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["media-1.api-sports.io", "media-2.api-sports.io", "media-3.api-sports.io"],
+    domains: ["media-1.api-sports.io", "media-2.api-sports.io", "media-3.api-sports.io", "media-4.api-sports.io"],
   },
 };
 
